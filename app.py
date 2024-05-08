6c900009d1bc37a40bc5cd1704c672e2909fc375
Lidia Aprilia Putri <126661491+Lidiaaprilia@users.noreply.github.com>
2024-05-08 13:17:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ df = pd.read_csv(url)
 
 # Sidebar
 with st.sidebar:
-    st.image('pollution.png')
+    st.image('pollution.png', width=270)
     
     st.title('🌡 Air Quality in Jakarta Panel')
 
